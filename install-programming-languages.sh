e3f045cbca88143599baf405e491893c2efc4d9b
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-09-20 09:01:03 +0200

    feat(rust): add commands to install toolchain and rust-analyzer

--- install-programming-languages.sh
+++ install-programming-languages.sh
@@ -6,3 +6,8 @@ sudo pacman -S --needed --noconfirm go
 
 # Install rustup for rust tool chain management
 sudo pacman -S --needed --noconfirm rustup
+# Installation of the stable toolchain
+rustup default stable
+
+# Installation of rust-analyzer
+rustup component add rust-analyzer rust-src
