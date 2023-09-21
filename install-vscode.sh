6a8b17b51f7646740b8d6a672b1c537e3d13e612
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-09-21 06:48:21 +0200

    feat: add vscode

---
+++ install-vscode.sh
@@ -0,0 +1,4 @@
+# Install Visual Studio Code from ArcoLinux repositories
+sudo pacman -S --needed --noconfirm visual-studio-code-bin
+# Install dependency for LiveShare extension
+paru -S --needed --noconfirm icu69-bin
