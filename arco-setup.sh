79e3920ff489871efe729533bcc5b7fb4abf9d52
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-04-20 11:23:47 +0200

    feat: updates

--- arco-setup.sh
+++ arco-setup.sh
@@ -2,10 +2,10 @@
 sudo pacman -Sy
 
 # Basic packages
-sudo pacman -S --noconfirm --needed bat curl fd flameshot fzf galculator go-yq jq meld neovim ripgrep starship stow tealdeer tmux tree unzip wget zoxide
+sudo pacman -S --noconfirm --needed bat chezmoi curl fd fzf go-yq jq meld neovim ripgrep starship tealdeer tmux tree unzip wget zellij zoxide
 
 # Git tools
-sudo pacman -S --noconfirm --needed git git-delta gitahead-bin github-cli lazygit
+sudo pacman -S --noconfirm --needed git git-delta github-cli lazygit
 
 # Shells
 sudo pacman -S --noconfirm --needed bash bash-completion
@@ -17,14 +17,8 @@ sudo pacman -S --noconfirm --needed docker docker-buildx docker-compose
 # Distrobox
 # paru -S --noconfirm --needed distrobox
 
-# Python pip
-sudo pacman -S --noconfirm --needed python-pip
-
 # Node manager (for neovim mason plugin)
-paru -S --noconfirm --needed fnm-bin
+yay -S --noconfirm --needed fnm-bin
 
 # Terraform
 sudo pacman -S --noconfirm --needed terraform
-
-# Install Vivaldi Web Browser
-sudo pacman -S --noconfirm --needed vivaldi vivaldi-ffmpeg-codecs
